f095a316e52989b5e6ed69b6f4e66c64eec2d875
JackM3 <71573684+JackM3@users.noreply.github.com>
2023-06-06 12:44:55 -0700

    Fix trigger mode usage
    It's not a function

--- CameraDriver.py
+++ CameraDriver.py
@@ -24,10 +24,11 @@ class CameraDriver:
         self.gd.ctrl.SetResolutionAndROI(1, 0, 0, 2048, 2048) # FullResolution=Yes, Left=Top=0, Width=Height=2048
     
     def getTriggerMode(self):
-        return self.gd.ctrl.LCMTriggerMode()
+        return self.gd.ctrl.LCMTriggerMode
         
     def setTriggerMode(self, triggerMode):
-        assert self.gd.ctrl.LCMTriggerMode(triggerMode)==triggerMode, 'Failed to set trigger mode'
+        self.gd.ctrl.LCMTriggerMode = triggerMode
+        assert self.gd.ctrl.LCMTriggerMode==triggerMode, 'Failed to set trigger mode'
     
     def setExposureAndGain(self, newExposure, newGain):
         # This class's setters check their own success, so no need to check again here
