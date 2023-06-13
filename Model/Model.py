e763200718d534d3b3f3b961abdc30464d20f492
Lawrence Sullivan <lawrence@loupe.team>
2023-06-13 13:54:15 -0700

    Add incoming start/abort/nextPixel commands

--- Model/Model.py
+++ Model/Model.py
@@ -149,7 +149,10 @@ class Model:
         "OpticsBoxBeamBlockCTemp" : BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.OpticsBoxBeamBlockCTemp"),
         "OpticsBoxSinkUpperTemp" : BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.OpticsBoxSinkUpperTemp"),
         "OpticsBoxSinkMiddleTemp" : BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.OpticsBoxSinkMiddleTemp"),
-        "OpticsBoxSinkLowerTemp" : BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.OpticsBoxSinkLowerTemp")}
+        "OpticsBoxSinkLowerTemp" : BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.OpticsBoxSinkLowerTemp"),
+        "StartTest": BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.StartTest"),
+        "AbortTest": BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.AbortTest"),
+        "ProceedToNextPixel": BNRopcuaTag(self.client, "ns=6;s=::AsGlobalPV:gOpcData_ToCalibApp.ProceedToNextPixel")}
 
         # definition of all the plc tags as a variable bound to the dictionary element
         # this is redundant to the dictionary but give the option to use dot operators to access the tags rather than strings
@@ -198,6 +201,9 @@ class Model:
         self.testStatusTag = self.plcTags["TestStatus"]
         self.userAccessLevelTag = self.plcTags["UserAccessLevel"]
         self.CurrentLUTIDTag = self.plcTags["CurrentLUTID"]
+        self.startTestTag = self.plcTags["StartTest"]
+        self.abortTestTag = self.plcTags["AbortTest"]
+        self.proceedToNextPixelTag = self.plcTags["ProceedToNextPixel"]
 
         ### Lookup Tables for Data Outputs #####
         self.testStatusTable = ["In Progress", "Passed", "High Power Failure", "Low Power Failure", "No Power Failure", "Untested", "", "", "", "", "Abort"]
@@ -217,11 +223,17 @@ class Model:
             self.testStatusTag._setAsUpdating()
             self.errorNumTag._setAsUpdating()
             self.heartBeatOutTag._setAsUpdating()
+            self.startTestTag._setAsUpdating()
+            self.abortTestTag._setAsUpdating()
+            self.proceedToNextPixelTag._setAsUpdating()
             self.userAccessLevelTag._setAsUpdating()
             self.CurrentLUTIDTag._setAsUpdating()
             self.ConfigValid._setAsUpdating()
             self.testStatusTag.attachReaction(self.testStatusReaction)
             self.heartBeatOutTag.attachReaction(self.heartBeatReaction)
+            self.startTestTag.attachReaction(self.startTestReaction)
+            self.abortTestTag.attachReaction(self.abortTestReaction)
+            self.proceedToNextPixelTag.attachReaction(self.proceedToNextPixelReaction)
             if self.FactoryNameTag.value == "VulcanOne":
                 MachineSettings._factoryID = "V1"
             else:
@@ -551,6 +563,23 @@ class Model:
     def heartBeatReaction(self):
         self.heartBeatIntag.setPlcValue(self.heartBeatOutTag.value + 1)
 
+    def startTestReaction(self):
+        start = self.startTestTag.value
+        if start == True:
+            self.logger.addNewLog("Test start command sent by PLC ")
+            self.startTest()
+
+    def abortTestReaction(self):
+        abort = self.abortTestTag.value
+        if abort == True:
+            self.logger.addNewLog("Test abort command sent by PLC ")
+            self.abortTest()
+        
+    def proceedToNextPixelReaction(self):
+        proceedToNextPixel = self.proceedToNextPixelTag.value
+        if proceedToNextPixel == True:
+            self.logger.addNewLog("Proceed to next pixel command sent by PLC ")
+            self.goToNextPixel()
 
 ############################################# ADDING REACTIONS ##############################################
 
