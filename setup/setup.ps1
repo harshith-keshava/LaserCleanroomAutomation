098b392d468896dd6a08b52cde0682c9a3974bd4
JackM3 <71573684+JackM3@users.noreply.github.com>
2023-06-16 13:36:49 -0700

    Adds pip upgrade to setup script
    pip 10.0.1 fails to install PyQt5 for me, and 23.1.2 succeeds

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -2,6 +2,7 @@
 Set-Location $AppRootPath
 py -3.7 -m venv venv
 .\venv\Scripts\activate
+python -m pip install --upgrade pip==23.1.2
 pip install -r .\setup\requirements.txt
 $DeskTopPath=[Environment]::GetFolderPath("Desktop")
 New-Item -ItemType SymbolicLink -Path $DeskTopPath -Name "LaserApplication.lnk" -Value "$AppRootPath\AutolaserCalibrationApplication.bat"
