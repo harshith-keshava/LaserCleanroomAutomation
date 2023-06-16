7f57c343b333075e7abc998b8d9a557056ba9934
JackM3 <71573684+JackM3@users.noreply.github.com>
2023-06-16 13:34:00 -0700

    Make setup.ps1 use relative paths
    Specifically, relative to the script's location,
    rather than wherever it's being called from

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -1,6 +1,7 @@
-﻿Set-Location C:\AutoLaserCalibration
+﻿$AppRootPath = "$PSScriptRoot\.."
+Set-Location $AppRootPath
 python -m venv venv
 .\venv\Scripts\activate
 pip install -r .\setup\requirements.txt
 $DeskTopPath=[Environment]::GetFolderPath("Desktop")
-New-Item -ItemType SymbolicLink -Path $DeskTopPath -Name "LaserApplication.lnk" -Value "C:\AutoLaserCalibration\AutolaserCalibrationApplication.bat"
+New-Item -ItemType SymbolicLink -Path $DeskTopPath -Name "LaserApplication.lnk" -Value "$AppRootPath\AutolaserCalibrationApplication.bat"
