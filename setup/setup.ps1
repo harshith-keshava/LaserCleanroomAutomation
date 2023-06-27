ed597fad93e54593ff12e811c4eba47aa3f74738
Lawrence Sullivan <lawrence@loupe.team>
2023-06-27 09:11:18 -0400

    Separate requirements by required pip version

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -2,7 +2,9 @@
 Set-Location $AppRootPath
 py -3.7 -m venv venv
 .\venv\Scripts\activate
+python -m pip install --upgrade pip==10.0.1
+pip install -r .\setup\requirements-pip-10-0-1.txt
 python -m pip install --upgrade pip==23.1.2
-pip install -r .\setup\requirements.txt
+pip install -r .\setup\requirements-pip-23-1-2.txt
 $DeskTopPath=[Environment]::GetFolderPath("Desktop")
 New-Item -ItemType SymbolicLink -Path $DeskTopPath -Name "LaserApplication.lnk" -Value "$AppRootPath\AutolaserCalibrationApplication.bat"
