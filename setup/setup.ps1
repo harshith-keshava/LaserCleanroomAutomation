8c551d79418271f63c62099248676481f782cbf2
JackM3 <71573684+JackM3@users.noreply.github.com>
2023-08-23 14:03:31 -0700

    Update setup to replace symbolic link w/ shortcut
    Also gives it an icon from the setup folder

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -7,4 +7,9 @@ pip install -r .\setup\requirements-pip-10-0-1.txt
 python -m pip install --upgrade pip==23.1.2
 pip install -r .\setup\requirements-pip-23-1-2.txt
 $DeskTopPath=[Environment]::GetFolderPath("Desktop")
-New-Item -ItemType SymbolicLink -Path $DeskTopPath -Name "LaserApplication.lnk" -Value "$AppRootPath\AutolaserCalibrationApplication.bat"
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WScriptObj.CreateShortcut("$DesktopPath\LaserApplication.lnk")
+$shortcut.TargetPath = "$AppRootPath\AutolaserCalibrationApplication.bat"
+$shortcut.IconLocation = "$AppRootPath\setup\laserbeam_laser_4058.ico"
+$shortcut.Save()
+#New-Item -ItemType SymbolicLink -Path $DeskTopPath -Name "LaserApplication.lnk" -Value "$AppRootPath\AutolaserCalibrationApplication.bat"
